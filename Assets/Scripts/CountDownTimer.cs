d367c650a1cb9fbde86226d48f64468727742409
Yokurt_321 <helen_xu2002@outlook.com>
2026-01-30 20:38:48 -0500

    Count down timer

---
+++ Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CountDownTimer : MonoBehaviour
+{
+    [SerializeField] private float StartSecond = 60f;
+    [SerializeField] private bool StartOnAwake = true;
+
+    [SerializeField] private TMP_Text timerText;
+
+    public UnityEvent onTimeOut;
+    public UnityEvent onReset;
+
+    private float remaining;
+    private bool running;
+
+    public float Remaining => remaining;
+
+    public bool isRunning => running;
+
+    private void Awake()
+    {
+        remaining = StartSecond;
+
+        UpdateUI();
+
+        if (StartOnAwake)
+        {
+            startTimer();
+        }
+    }
+
+    private void Update()
+    {
+        if(!isRunning)
+        {
+            return;
+        }
+
+        remaining -= Time.deltaTime;
+
+        if (remaining <= 0f)
+        {
+            remaining = 0f;
+            running = false;
+            UpdateUI();
+            onTimeOut?.Invoke();
+            return;
+        }
+
+        UpdateUI();
+    }
+
+    private void startTimer()
+    {
+        running = true;
+    }
+
+    public void StopTimer()
+    {
+        running = false;
+    }
+
+    public void ResetTimer()
+    {
+        remaining = StartSecond;
+        UpdateUI();
+        onReset?.Invoke();
+    }
+
+    public void ResetTimer(float newStartSecond)
+    {
+        StartSecond = Mathf.Max(0f, newStartSecond);
+        ResetTimer();
+    }
+
+    private void UpdateUI()
+    {
+        if (timerText == null)
+        return;
+
+        int sec = Mathf.CeilToInt(remaining);
+        int m = sec/60;
+        int s = sec%60;
+        timerText.text = $"{m:00}:{s:00}";
+    }
+
+}
